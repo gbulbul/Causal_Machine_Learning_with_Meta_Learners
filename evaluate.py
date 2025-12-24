3e349ef28d2eabed1708d5b58a14c8e2533ee116
Gul Bulbul <79763247+gbulbul@users.noreply.github.com>
2025-12-24 01:10:54 -0800

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -6,6 +6,9 @@ import matplotlib.pyplot as plt
 def rmse(x, y):
     return np.sqrt(np.mean((x - y) ** 2))
 
+def mae(y_true, y_pred):
+    return np.mean(np.abs(y_true - y_pred))
+
 if __name__ == "__main__":
 
     # simulate data
