12e8b64698e3b557187a1e68178355e55cb79c52
Gul Bulbul <79763247+gbulbul@users.noreply.github.com>
2025-12-24 01:24:27 -0800

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,7 +1,4 @@
 import numpy as np
-from simulate_data import simulate_causal_data
-from meta_learners import s_learner, t_learner
-import matplotlib.pyplot as plt
 
 def rmse(x, y):
     return np.sqrt(np.mean((x - y) ** 2))
